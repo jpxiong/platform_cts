e56ee8d1009dabce1c0897fce0efc84b6a5e04ff
Scott Su <>
2009-04-16 12:26:03 -0700

    AI 146526: am: CL 146102 am: CL 146100 CTS: add test case for util.EventLogTags, Log, SparseArray, SparseBooleanArray, SparseIntArray, StateSet and TimingLogger.   Original author: sus   Merged from: //branches/cupcake/...   Original author: android-build
    Automated import of CL 146526

---
+++ tests/tests/util/src/android/util/cts/EventLogTagsTest.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright (C) 2008 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package android.util.cts;
+
+import android.test.AndroidTestCase;
+import android.util.EventLogTags;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+import dalvik.annotation.TestTargets;
+import dalvik.annotation.TestStatus;
+import dalvik.annotation.TestTargetNew;
+import dalvik.annotation.TestLevel;
+import dalvik.annotation.TestTargetClass;
+
+/**
+ * Test class android.util.EventLogTags.
+ */
+@TestTargetClass(EventLogTags.class)
+public class EventLogTagsTest extends AndroidTestCase {
+
+    private final static String TAGS_FILE = "/etc/event-log-tags";
+
+    @TestTargets({
+        @TestTargetNew(
+            level = TestLevel.COMPLETE,
+            notes = "Test EvnetLogTags operations.",
+            method = "EventLogTags",
+            args = {}
+        ),
+        @TestTargetNew(
+            level = TestLevel.COMPLETE,
+            notes = "Test EvnetLogTags operations.",
+            method = "EventLogTags",
+            args = {java.io.BufferedReader.class}
+        ),
+        @TestTargetNew(
+            level = TestLevel.COMPLETE,
+            notes = "Test EvnetLogTags operations.",
+            method = "get",
+            args = {int.class}
+        ),
+        @TestTargetNew(
+            level = TestLevel.COMPLETE,
+            notes = "Test EvnetLogTags operations.",
+            method = "get",
+            args = {java.lang.String.class}
+        )
+    })
+    public void testEventLogTagsOp() throws Exception {
+        EventLogTags tags = new EventLogTags();
+        assertEquals(42, tags.get("answer").mTag);
+        assertEquals(314, tags.get("pi").mTag);
+        assertEquals(2718, tags.get("e").mTag);
+        assertEquals("answer", tags.get(42).mName);
+        assertEquals("pi", tags.get(314).mName);
+        assertEquals("e", tags.get(2718).mName);
+
+        tags = new EventLogTags(new BufferedReader(new FileReader(TAGS_FILE), 256));
+        assertEquals(42, tags.get("answer").mTag);
+        assertEquals(314, tags.get("pi").mTag);
+        assertEquals(2718, tags.get("e").mTag);
+        assertEquals("answer", tags.get(42).mName);
+        assertEquals("pi", tags.get(314).mName);
+        assertEquals("e", tags.get(2718).mName);
+    }
+}
