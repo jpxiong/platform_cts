89a0970c05a889e17470de7dbdd5b762fc78b248
Scott Su <>
2009-04-23 22:02:53 -0700

    AI 147594: CTS: add test cases for android.os.TokenWatcher
    Automated import of CL 147594

--- tests/Android.mk
+++ tests/Android.mk
@@ -19,6 +19,7 @@ LOCAL_MODULE_TAGS := tests
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)\
               src/android/app/cts/ISecondary.aidl\
+              src/android/os/cts/IEmptyService.aidl
 
 LOCAL_JAVA_LIBRARIES := android.test.runner
 
