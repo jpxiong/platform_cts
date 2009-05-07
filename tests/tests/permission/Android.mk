5671dd66999a0b43ca078458e3db59b3263d4fa1
Phil Dubach <nobody@android.com>
2009-05-07 10:34:41 -0700

    AI 148455: CTS: Change tests in Android plan to use new InstrumentationCtsTestRunner   BUG=1537738
    Automated import of CL 148455

--- tests/tests/permission/Android.mk
+++ tests/tests/permission/Android.mk
@@ -25,6 +25,8 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_PACKAGE_NAME := CtsPermissionTestCases
 
+LOCAL_INSTRUMENTATION_FOR := android.core.tests.runner
+
 LOCAL_SDK_VERSION := current
 
 include $(BUILD_PACKAGE)
