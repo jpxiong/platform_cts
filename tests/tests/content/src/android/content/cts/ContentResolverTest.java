e15a994531cd2f8db19ce0bdd8baed98ba2bfef3
Brett Chabot <brettchabot@google.com>
2009-07-20 17:32:32 -0700

    Cts content tests cleanup.
    Remove tests for hidden APIs SyncResult, SyncContext and SyncStats.
    Fix IntentTest.testFillIn, and rename the IntentTest constants.

--- tests/tests/content/src/android/content/cts/ContentResolverTest.java
+++ tests/tests/content/src/android/content/cts/ContentResolverTest.java
@@ -641,9 +641,11 @@ public class ContentResolverTest extends AndroidTestCase {
 
         try {
             mContentResolver.registerContentObserver(null, false, mco);
-            fail("did not throw NullPointerException when uri is null.");
+            fail("did not throw NullPointerException or IllegalArgumentException when uri is null.");
         } catch (NullPointerException e) {
             //expected.
+        } catch (IllegalArgumentException e) {
+            // also expected
         }
 
         try {
@@ -677,12 +679,6 @@ public class ContentResolverTest extends AndroidTestCase {
         mContentResolver.notifyChange(TABLE1_URI, mco);
         assertTrue(mco.hadOnChanged());
 
-        try {
-            mContentResolver.notifyChange(null, mco);
-            fail("did not throw NullPointerException when uri is null.");
-        } catch (NullPointerException e) {
-            //expected.
-        }
         mContentResolver.unregisterContentObserver(mco);
     }
 
@@ -702,12 +698,6 @@ public class ContentResolverTest extends AndroidTestCase {
         mContentResolver.notifyChange(TABLE1_URI, mco, false);
         assertTrue(mco.hadOnChanged());
 
-        try {
-            mContentResolver.notifyChange(null, mco, false);
-            fail("did not throw NullPointerException when uri is null.");
-        } catch (NullPointerException e) {
-            //expected.
-        }
         mContentResolver.unregisterContentObserver(mco);
     }
 
