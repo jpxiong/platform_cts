388276c1e157e51c9a280205734bea815eadf59d
Dan Bornstein <danfuzz@android.com>
2009-11-18 17:16:33 -0800

    A very very basic test of JNI linkage.
    It needs to be expanded to cover many more variants of method calling,
    as well as covering the use of all the JNI functions from C or C++.
    
    Removed the annotations from the example test while I was at it, since
    apparently they're not recommended practice anymore.
    
    Change-Id: Ic90361e66c06afa7edeb6373908b55ebf7d105a1

---
+++ tests/tests/jni/src/android/jni/cts/StaticNonce.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2009 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package android.jni.cts;
+
+/**
+ * Class with a bunch of native static methods. These methods are called by
+ * the various tests in {@link JniStaticTest}.
+ */
+public class StaticNonce {
+    static {
+        System.loadLibrary("jnitest");
+    }
+
+    // See JniStaticTest for the expected behavior of these methods.
+
+    public static native void nop();
+    public static native int returnInt();
+    public static native double returnDouble();
+}
