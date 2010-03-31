f7301dad2df504ab0ac0a1303469d5d06552daa4
Brett Chabot <brettchabot@android.com>
2010-03-30 19:48:41 -0700

    Add cts hardware to tests build and annotate tests.
    Bug 2557864
    
    Change-Id: I5bb406c9504149950c52c949dd7e28013db28fb0

--- tests/tests/hardware/Android.mk
+++ tests/tests/hardware/Android.mk
@@ -16,10 +16,7 @@ LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
 
-# don't include this package in any target
-LOCAL_MODULE_TAGS := optional
-# and when built explicitly put it in the data partition
-LOCAL_MODULE_PATH := $(TARGET_OUT_DATA_APPS)
+LOCAL_MODULE_TAGS := tests
 
 LOCAL_JAVA_LIBRARIES := android.test.runner
 
