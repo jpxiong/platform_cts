320a7a1495cfeb3823c7c2877b1e00efbeb7af61
Brian Muramatsu <btmura@android.com>
2010-07-08 16:37:21 -0700

    Add CtsTestCaseList.mk to Build CTS
    Bug 2732160
    
    Extract the test case list from build/core/tasks/cts.mk and put
    them in a CtsTestCaseList.mk file. This change will make it
    easier to develop CTS in the open, since adding test packages
    and applications will no longer require multiple project changes.
    
    Another alternative was to move the entire cts.mk file to the
    CTS project but since the Makefile is in flux in the master branch
    so much that I've decided to simplify things by only including
    the test case list to avoid merge issues.
    
    Also I decided to keep this in a separate Makefile rather than
    stuffing the definitions in the CTS Android.mk. I'm thinking
    its better to be more explicit with an include statement
    rather than being implicitly defined in the Android.mk.
    
    Change-Id: I7476ed02271d03fe1c6082e3e6f3013004d8ceae

---
+++ CtsTestCaseList.mk
@@ -0,0 +1,66 @@
+# Copyright (C) 2010 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+CTS_SECURITY_APPS_LIST := \
+	CtsAppAccessData \
+	CtsAppWithData \
+	CtsInstrumentationAppDiffCert \
+	CtsPermissionDeclareApp \
+	CtsSharedUidInstall \
+	CtsSharedUidInstallDiffCert \
+	CtsSimpleAppInstall \
+	CtsSimpleAppInstallDiffCert \
+	CtsTargetInstrumentationApp \
+	CtsUsePermissionDiffCert
+
+CTS_TEST_CASE_LIST := \
+	TestDeviceSetup \
+	CtsTestStubs \
+	CtsAccessibilityServiceTestCases \
+	CtsAccountManagerTestCases \
+	CtsAppTestCases \
+	CtsBluetoothTestCases \
+	CtsContentTestCases \
+	CtsDatabaseTestCases \
+	CtsDelegatingAccessibilityService \
+	CtsDpiTestCases \
+	CtsDpiTestCases2 \
+	CtsExampleTestCases \
+	CtsGestureTestCases \
+	CtsGraphicsTestCases \
+	CtsHardwareTestCases \
+	CtsJniTestCases \
+	CtsLocationTestCases \
+	CtsMediaTestCases \
+	CtsOsTestCases \
+	CtsPermissionTestCases \
+	CtsPermission2TestCases \
+	CtsProviderTestCases \
+	CtsSpeechTestCases \
+	CtsTelephonyTestCases \
+	CtsTextTestCases \
+	CtsUtilTestCases \
+	CtsViewTestCases \
+	CtsWebkitTestCases \
+	CtsWidgetTestCases \
+	CtsNetTestCases \
+	SignatureTest \
+	CtsPerformanceTestCases \
+	CtsPerformance2TestCases \
+	CtsPerformance3TestCases \
+	CtsPerformance4TestCases \
+	CtsPerformance5TestCases \
+	ApiDemos \
+	ApiDemosReferenceTest \
+	$(CTS_SECURITY_APPS_LIST)
