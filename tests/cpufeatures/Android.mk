49ed3b8174df85cdb0b319709bb31a2becb2e4b0
Brian Muramatsu <btmura@android.com>
2010-08-25 13:18:06 -0700

    Fix Eclair sim-eng Build
    Change-Id: I60c192b8ea347afb4ed14e621d174a844f004c11

--- tests/cpufeatures/Android.mk
+++ tests/cpufeatures/Android.mk
@@ -4,7 +4,9 @@ LOCAL_PATH := $(call my-dir)
 # function. It will only be called by the main
 # library if we detect ARMv7 through a system property.
 #
+ifneq ($(TARGET_SIMULATOR),true)
 include $(CLEAR_VARS)
 LOCAL_MODULE := cpufeatures
 LOCAL_SRC_FILES := cpu-features.c
 include $(BUILD_STATIC_LIBRARY)
+endif
