c4cd1326d19c7b02bfe90e698066dd295c411bd2
Brian Muramatsu <btmura@android.com>
2010-08-26 15:30:02 -0700

    Fix CameraTest#testAccessParameters Assertion
    Issue 10800
    
    Use a float assertEquals and swap expected and actual parameters
    around.
    
    Change-Id: I791947c78471d8fdb992843ca66e09333ba0ddec

--- tests/tests/hardware/src/android/hardware/cts/CameraTest.java
+++ tests/tests/hardware/src/android/hardware/cts/CameraTest.java
@@ -16,7 +16,6 @@
 
 package android.hardware.cts;
 
-import dalvik.annotation.BrokenTest;
 import dalvik.annotation.TestLevel;
 import dalvik.annotation.TestTargetClass;
 import dalvik.annotation.TestTargetNew;
@@ -38,8 +37,8 @@ import android.os.ConditionVariable;
 import android.os.Environment;
 import android.os.Looper;
 import android.test.ActivityInstrumentationTestCase2;
-import android.test.suitebuilder.annotation.LargeTest;
 import android.test.UiThreadTest;
+import android.test.suitebuilder.annotation.LargeTest;
 import android.util.Log;
 import android.view.SurfaceHolder;
 
@@ -633,7 +632,7 @@ public class CameraTest extends ActivityInstrumentationTestCase2<CameraStubActiv
         int min = parameters.getMinExposureCompensation();
         float step = parameters.getExposureCompensationStep();
         if (max == 0 && min == 0) {
-            assertEquals(step, 0);
+            assertEquals(0f, step, 0.000001f);
             return;
         }
         assertTrue(step > 0);
