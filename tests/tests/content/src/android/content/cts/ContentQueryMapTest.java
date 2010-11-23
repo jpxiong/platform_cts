ab10ac54b5743cb640e245949a655fb8919cdc77
Brian Muramatsu <btmura@google.com>
2010-11-23 12:22:45 -0800

    Revert "Fix ContentQueryMapTest#testGetRows"
    This reverts commit 55111a2fa73d0cfb6d21929801bde8fa048193fe.
    
    ...and now it no longer throws the exception.

--- tests/tests/content/src/android/content/cts/ContentQueryMapTest.java
+++ tests/tests/content/src/android/content/cts/ContentQueryMapTest.java
@@ -147,11 +147,9 @@ public class ContentQueryMapTest extends AndroidTestCase {
         mContentQueryMap = new ContentQueryMap(mCursor, DummyProvider.NAME, false, new Handler());
         rows = mContentQueryMap.getRows();
         assertFalse(rows.containsKey(NAME0));
-        try {
-            mContentQueryMap.requery();
-            fail("Should throw an IllegalStateException about requerying a closed cursor.");
-        } catch (IllegalStateException requeryAlreadyClosedCursor) {
-        }
+        mContentQueryMap.requery();
+        rows = mContentQueryMap.getRows();
+        assertFalse(rows.containsKey(NAME0));
     }
 
     @TestTargets({
