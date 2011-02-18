04d3b73e546c8317b1d099cb66e6ffa91eec50bb
David Krause <david.krause@motorola.com>
2011-02-18 00:08:09 -0600

    Add QHD to the supported ScreenConfiguration list
    Change-Id: I29f35994b722ff35a0913212f295dc51ca80152d

--- tests/tests/dpi/src/android/dpi/cts/ConfigurationTest.java
+++ tests/tests/dpi/src/android/dpi/cts/ConfigurationTest.java
@@ -179,6 +179,7 @@ public class ConfigurationTest extends AndroidTestCase {
      * HVGA      | medium (141-190) | normal
      * WVGA      | high (191-250)   | normal
      * FWVGA     | high (191-250)   | normal
+     * QHD       | high (191-250)   | normal
      * WSVGA     | high (191-250)   | large
 
      * VGA       | medium (141-190) | large
@@ -200,6 +201,8 @@ public class ConfigurationTest extends AndroidTestCase {
         new ScreenConfiguration(640, 480, Density.HIGH, Configuration.SCREENLAYOUT_SIZE_NORMAL, true),
         // FWVGA     | high (191-250)   | normal
         new ScreenConfiguration(864, 480, Density.HIGH, Configuration.SCREENLAYOUT_SIZE_NORMAL),
+        // QHD     | high (191-250)   | normal
+        new ScreenConfiguration(960, 540, Density.HIGH, Configuration.SCREENLAYOUT_SIZE_NORMAL),
         // WSVGA     | high (191-250)   | large
         new ScreenConfiguration(1024, 600, Density.HIGH, Configuration.SCREENLAYOUT_SIZE_LARGE),
 
