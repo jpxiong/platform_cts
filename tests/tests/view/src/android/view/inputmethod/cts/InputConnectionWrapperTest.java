079769fe91d28a588f49eb72247aec16199c669d
satok <satok@google.com>
2011-03-16 17:21:55 +0900

    Fix build breakage in CTS
    Change-Id: I0fce1fde5ae0db6daacdb00297404861ac3178e6

--- tests/tests/view/src/android/view/inputmethod/cts/InputConnectionWrapperTest.java
+++ tests/tests/view/src/android/view/inputmethod/cts/InputConnectionWrapperTest.java
@@ -22,8 +22,10 @@ import dalvik.annotation.TestTargetNew;
 import dalvik.annotation.TestTargets;
 
 import android.os.Bundle;
+import android.os.IBinder;
 import android.test.AndroidTestCase;
 import android.text.TextUtils;
+import android.text.style.CorrectionSpan;
 import android.view.KeyEvent;
 import android.view.inputmethod.CompletionInfo;
 import android.view.inputmethod.CorrectionInfo;
@@ -224,6 +226,7 @@ public class InputConnectionWrapperTest extends AndroidTestCase {
         public boolean isSendKeyEventCalled;
         public boolean isSetComposingTextCalled;
         public boolean isSetComposingRegionCalled;
+        public boolean isSetCorrectionSpanCalled;
         public boolean isSetSelectionCalled;
 
         public boolean beginBatchEdit() {
@@ -323,6 +326,12 @@ public class InputConnectionWrapperTest extends AndroidTestCase {
 
         public boolean setComposingRegion(int start, int end) {
             isSetComposingRegionCalled = true;
+            return false;
+        }
+
+        public boolean setCorrectionSpan(IBinder token, CorrectionSpan correctionSpan, int start,
+                int end, int flags) {
+            isSetCorrectionSpanCalled = true;
             return false;
         }
 
