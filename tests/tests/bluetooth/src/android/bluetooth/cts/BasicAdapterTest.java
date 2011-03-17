70adf80377bdfdfc183f04eaf88608779d592a38
Jason Bayer <bayer@google.com>
2011-03-16 20:22:04 -0700

    Bypass bluetooth tests if not present
    Tested on gtv and stingray
    
    Change-Id: Ifda7977248bcb26617bd3fde12efde359a072227

--- tests/tests/bluetooth/src/android/bluetooth/cts/BasicAdapterTest.java
+++ tests/tests/bluetooth/src/android/bluetooth/cts/BasicAdapterTest.java
@@ -19,30 +19,41 @@ package android.bluetooth.cts;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothServerSocket;
+import android.content.pm.PackageManager;
+import android.test.AndroidTestCase;
 
 import java.io.IOException;
 import java.util.Set;
 import java.util.UUID;
 
-import junit.framework.TestCase;
-
 /**
  * Very basic test, just of the static methods of {@link
  * BluetoothAdapter}.
  */
-public class BasicAdapterTest extends TestCase {
+public class BasicAdapterTest extends AndroidTestCase {
     private static final int DISABLE_TIMEOUT = 5000;  // ms timeout for BT disable
     private static final int ENABLE_TIMEOUT = 10000;  // ms timeout for BT enable
     private static final int POLL_TIME = 100;         // ms to poll BT state
 
+    private boolean mHasBluetooth;
+
+    public void setUp() throws Exception {
+        super.setUp();
+
+        mHasBluetooth = getContext().getPackageManager().hasSystemFeature(
+                PackageManager.FEATURE_BLUETOOTH);
+    }
+
     public void test_getDefaultAdapter() {
         /*
          * Note: If the target doesn't support Bluetooth at all, then
-         * this method will return null. The assumption here is that
-         * you won't bother running this test on a target that doesn't
-         * purport to support Bluetooth.
+         * this method should return null.
          */
-        assertNotNull(BluetoothAdapter.getDefaultAdapter());
+        if (mHasBluetooth) {
+            assertNotNull(BluetoothAdapter.getDefaultAdapter());
+        } else {
+            assertNull(BluetoothAdapter.getDefaultAdapter());
+        }
     }
 
     public void test_checkBluetoothAddress() {
@@ -105,6 +116,10 @@ public class BasicAdapterTest extends TestCase {
 
     /** Checks enable(), disable(), getState(), isEnabled() */
     public void test_enableDisable() {
+        if (!mHasBluetooth) {
+            // Skip the test if bluetooth is not present.
+            return;
+        }
         BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();
 
         for (int i=0; i<5; i++) {
@@ -114,6 +129,10 @@ public class BasicAdapterTest extends TestCase {
     }
 
     public void test_getAddress() {
+        if (!mHasBluetooth) {
+            // Skip the test if bluetooth is not present.
+            return;
+        }
         BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();
         enable(adapter);
 
@@ -121,6 +140,10 @@ public class BasicAdapterTest extends TestCase {
     }
 
     public void test_getName() {
+        if (!mHasBluetooth) {
+            // Skip the test if bluetooth is not present.
+            return;
+        }
         BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();
         enable(adapter);
 
@@ -129,6 +152,10 @@ public class BasicAdapterTest extends TestCase {
     }
 
     public void test_getBondedDevices() {
+        if (!mHasBluetooth) {
+            // Skip the test if bluetooth is not present.
+            return;
+        }
         BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();
         enable(adapter);
 
@@ -140,6 +167,10 @@ public class BasicAdapterTest extends TestCase {
     }
 
     public void test_getRemoteDevice() {
+        if (!mHasBluetooth) {
+            // Skip the test if bluetooth is not present.
+            return;
+        }
         // getRemoteDevice() should work even with Bluetooth disabled
         BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();
         disable(adapter);
@@ -161,6 +192,10 @@ public class BasicAdapterTest extends TestCase {
     }
 
     public void test_listenUsingRfcommWithServiceRecord() throws IOException {
+        if (!mHasBluetooth) {
+            // Skip the test if bluetooth is not present.
+            return;
+        }
         BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();
         enable(adapter);
 
