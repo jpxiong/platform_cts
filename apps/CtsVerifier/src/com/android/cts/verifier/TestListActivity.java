f942683666d3ae59e44e69ed0ab77fe9db71f207
Brian Muramatsu <btmura@android.com>
2011-06-17 15:43:59 -0700

    CTS Verifier Sub Tests Implementation
    Refactor the Bluetooth test to use the same test results storage
    as the main test list. This will make it easy for future tests
    to have subtests like the Bluetooth test one does. Also it will
    be possible to send a report that has the results for the sub
    tests since all the data is in one place.
    
    Create subtests by adding a meta data with the tag "test_parent"
    to specify the class that this test belongs to.
    
    Change-Id: Id7ec5da311751bb8c1af7915137dacc89286a23c

--- apps/CtsVerifier/src/com/android/cts/verifier/TestListActivity.java
+++ apps/CtsVerifier/src/com/android/cts/verifier/TestListActivity.java
@@ -19,12 +19,8 @@ package com.android.cts.verifier;
 import com.android.cts.verifier.TestListAdapter.TestListItem;
 
 import android.app.ListActivity;
-import android.content.ContentResolver;
-import android.content.ContentValues;
 import android.content.Intent;
-import android.database.ContentObserver;
 import android.os.Bundle;
-import android.os.Handler;
 import android.text.ClipboardManager;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -37,17 +33,14 @@ import android.widget.Toast;
 public class TestListActivity extends ListActivity {
 
     private static final int LAUNCH_TEST_REQUEST_CODE = 1;
+    private TestListAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        TestListAdapter adapter = new TestListAdapter(this);
-        setListAdapter(adapter);
-
-        TestResultContentObserver observer = new TestResultContentObserver(adapter);
-        ContentResolver resolver = getContentResolver();
-        resolver.registerContentObserver(TestResultsProvider.RESULTS_CONTENT_URI, true, observer);
+        mAdapter = new TestListAdapter(this, null);
+        setListAdapter(mAdapter);
+        mAdapter.loadTestResults();
     }
 
     /** Launch the activity when its {@link ListView} item is clicked. */
@@ -59,14 +52,8 @@ public class TestListActivity extends ListActivity {
     }
 
     private Intent getIntent(int position) {
-        TestListAdapter adapter = getListAdapter();
-        TestListItem item = adapter.getItem(position);
+        TestListItem item = mAdapter.getItem(position);
         return item.intent;
-    }
-
-    @Override
-    public TestListAdapter getListAdapter() {
-        return (TestListAdapter) super.getListAdapter();
     }
 
     @Override
@@ -85,18 +72,7 @@ public class TestListActivity extends ListActivity {
     private void handleLaunchTestResult(int resultCode, Intent data) {
         if (resultCode == RESULT_OK) {
             TestResult testResult = TestResult.fromActivityResult(resultCode, data);
-            ContentValues values = new ContentValues(2);
-            values.put(TestResultsProvider.COLUMN_TEST_RESULT, testResult.getResult());
-            values.put(TestResultsProvider.COLUMN_TEST_NAME, testResult.getName());
-
-            ContentResolver resolver = getContentResolver();
-            int numUpdated = resolver.update(TestResultsProvider.RESULTS_CONTENT_URI, values,
-                    TestResultsProvider.COLUMN_TEST_NAME + " = ?",
-                    new String[] {testResult.getName()});
-
-            if (numUpdated == 0) {
-                resolver.insert(TestResultsProvider.RESULTS_CONTENT_URI, values);
-            }
+            mAdapter.setTestResult(testResult);
         }
     }
 
@@ -128,13 +104,12 @@ public class TestListActivity extends ListActivity {
     }
 
     private void handleClearItemSelected() {
-        ContentResolver resolver = getContentResolver();
-        resolver.delete(TestResultsProvider.RESULTS_CONTENT_URI, "1", null);
+        mAdapter.clearTestResults();
         Toast.makeText(this, R.string.test_results_cleared, Toast.LENGTH_SHORT).show();
     }
 
     private void handleCopyItemSelected() {
-        TestResultsReport report = new TestResultsReport(this, getListAdapter());
+        TestResultsReport report = new TestResultsReport(this, mAdapter);
         ClipboardManager clipboardManager = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
         clipboardManager.setText(report.getBody());
         Toast.makeText(this, R.string.test_results_copied, Toast.LENGTH_SHORT).show();
@@ -144,31 +119,9 @@ public class TestListActivity extends ListActivity {
         Intent target = new Intent(Intent.ACTION_SEND);
         target.setType("text/plain");
 
-        TestResultsReport report = new TestResultsReport(this, getListAdapter());
+        TestResultsReport report = new TestResultsReport(this, mAdapter);
         target.putExtra(Intent.EXTRA_SUBJECT, report.getSubject());
         target.putExtra(Intent.EXTRA_TEXT, report.getBody());
         startActivity(Intent.createChooser(target, getString(R.string.share_test_results)));
-    }
-
-    /**
-     * {@link ContentResolver} that refreshes the {@link TestListAdapter} and thus
-     * the {@link ListView} when the test results change.
-     */
-    private static class TestResultContentObserver extends ContentObserver {
-
-        private final TestListAdapter mAdapter;
-
-        public TestResultContentObserver(TestListAdapter adapter) {
-            super(new Handler());
-            this.mAdapter = adapter;
-        }
-
-        @Override
-        public void onChange(boolean selfChange) {
-            super.onChange(selfChange);
-
-            // TODO: Could be improved by just refreshing the particular test result.
-            mAdapter.refreshTestResults();
-        }
     }
 }
