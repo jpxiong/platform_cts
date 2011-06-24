44ed288d5ab8437590c98631a6c65d36516cbf06
Stephen Hines <srhines@google.com>
2011-06-24 13:52:47 -0700

    Add type-checking tests for RS.
    BUG=4203264
    
    Change-Id: Ifd979cdebd2947a0fdf2ebf7e417a216893af49b

---
+++ tests/src/android/renderscript/cts/fe_f64_2.rs
@@ -0,0 +1,8 @@
+#pragma version(1)
+#pragma rs java_package_name(android.renderscript.cts)
+
+void root(const double2 *ain, double2 *aout) {
+    aout[0].x = ain[0].x + 1.0;
+    aout[0].y = ain[0].y + 1.0;
+    return;
+}
---
+++ tests/src/android/renderscript/cts/fe_i8_4.rs
@@ -0,0 +1,10 @@
+#pragma version(1)
+#pragma rs java_package_name(android.renderscript.cts)
+
+void root(const char4 *ain, uchar4 *aout) {
+    aout[0].x = ain[0].x + 1;
+    aout[0].y = ain[0].y + 1;
+    aout[0].z = ain[0].z + 1;
+    aout[0].w = ain[0].w + 1;
+    return;
+}
