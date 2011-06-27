82ab52aeb917ed9a7f19bcc52b0cee83580814df
Brian Muramatsu <btmura@android.com>
2011-06-27 14:02:12 -0700

    CTS Verifier XML Reports
    Bug 4878013
    
    1. Add a version name and code to the CTS Verifier. Show the
       version name on the welcome screen.
    
    2. Share a XML report instead of a plain text report. See
       TestResultsReport for the format.
    
    Change-Id: I10d21b7ea4d468472fc15866d7259e456d6741a6

--- apps/CtsVerifier/src/com/android/cts/verifier/TestResultsReport.java
+++ apps/CtsVerifier/src/com/android/cts/verifier/TestResultsReport.java
@@ -18,90 +18,131 @@ package com.android.cts.verifier;
 
 import com.android.cts.verifier.TestListAdapter.TestListItem;
 
-import android.content.Context;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.content.pm.PackageManager.NameNotFoundException;
-import android.os.Build;
+import org.xmlpull.v1.XmlSerializer;
 
-/** Plain text report of the current test results. */
+import android.content.Context;
+import android.os.Build;
+import android.util.Xml;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+/**
+ * XML text report of the current test results.
+ * <p>
+ * Sample:
+ * <pre>
+ * <?xml version='1.0' encoding='utf-8' standalone='yes' ?>
+ * <test-results-report report-version="1" creation-time="Tue Jun 28 11:04:10 PDT 2011">
+ *   <verifier-info version-name="2.3_r4" version-code="2" />
+ *   <device-info>
+ *     <build-info fingerprint="google/soju/crespo:2.3.4/GRJ22/121341:user/release-keys" />
+ *   </device-info>
+ *   <test-results>
+ *     <test title="Audio Quality Verifier" class-name="com.android.cts.verifier.audioquality.AudioQualityVerifierActivity" result="not-executed" />
+ *     <test title="Hardware/Software Feature Summary" class-name="com.android.cts.verifier.features.FeatureSummaryActivity" result="fail" />
+ *     <test title="Bluetooth Test" class-name="com.android.cts.verifier.bluetooth.BluetoothTestActivity" result="fail" />
+ *     <test title="SUID File Scanner" class-name="com.android.cts.verifier.suid.SuidFilesActivity" result="not-executed" />
+ *     <test title="Accelerometer Test" class-name="com.android.cts.verifier.sensors.AccelerometerTestActivity" result="pass" />
+ *   </test-results>
+ * </test-results-report>
+ * </pre>
+ */
 class TestResultsReport {
+
+    /** Version of the test report. Increment whenever adding new tags and attributes. */
+    private static final int REPORT_VERSION = 1;
+
+    /** Format of the report's creation time. Maintain the same format at CTS. */
+    private static DateFormat DATE_FORMAT =
+            new SimpleDateFormat("EEE MMM dd HH:mm:ss z yyyy", Locale.ENGLISH);
+
+    private static final String TEST_RESULTS_REPORT_TAG = "test-results-report";
+    private static final String VERIFIER_INFO_TAG = "verifier-info";
+    private static final String DEVICE_INFO_TAG = "device-info";
+    private static final String BUILD_INFO_TAG = "build-info";
+    private static final String TEST_RESULTS_TAG = "test-results";
+    private static final String TEST_TAG = "test";
 
     private final Context mContext;
 
     private final TestListAdapter mAdapter;
 
-    private final String mVersionName;
-
     TestResultsReport(Context context, TestListAdapter adapter) {
         this.mContext = context;
         this.mAdapter = adapter;
-        this.mVersionName = getVersionName(context);
     }
 
-    private static String getVersionName(Context context) {
-        try {
-            PackageManager packageManager = context.getPackageManager();
-            PackageInfo info = packageManager.getPackageInfo(context.getPackageName(), 0);
-            return info.versionName;
-        } catch (NameNotFoundException e) {
-            throw new RuntimeException("Could not get find package information for "
-                    + context.getPackageName());
-        }
+    String getType() {
+        return "application/xml";
     }
 
     String getSubject() {
-        return new StringBuilder()
-                .append(mContext.getString(R.string.subject_header, mVersionName))
-                .append(' ')
-                .append(Build.FINGERPRINT)
-                .toString();
+        return mContext.getString(R.string.subject_header,
+                Version.getVersionName(mContext),
+                Build.FINGERPRINT);
     }
 
-    String getBody() {
-        StringBuilder builder = new StringBuilder()
-                .append(mContext.getString(R.string.body_header, mVersionName))
-                .append("\n\n")
-                .append(Build.FINGERPRINT)
-                .append("\n\n");
+    String getBody() throws IllegalArgumentException, IllegalStateException, IOException {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
 
+        XmlSerializer xml = Xml.newSerializer();
+        xml.setOutput(outputStream, "utf-8");
+        xml.setFeature("http://xmlpull.org/v1/doc/features.html#indent-output", true);
+        xml.startDocument("utf-8", true);
+
+        xml.startTag(null, TEST_RESULTS_REPORT_TAG);
+        xml.attribute(null, "report-version", Integer.toString(REPORT_VERSION));
+        xml.attribute(null, "creation-time", DATE_FORMAT.format(new Date()));
+
+        xml.startTag(null, VERIFIER_INFO_TAG);
+        xml.attribute(null, "version-name", Version.getVersionName(mContext));
+        xml.attribute(null, "version-code", Integer.toString(Version.getVersionCode(mContext)));
+        xml.endTag(null, VERIFIER_INFO_TAG);
+
+        xml.startTag(null, DEVICE_INFO_TAG);
+        xml.startTag(null, BUILD_INFO_TAG);
+        xml.attribute(null, "fingerprint", Build.FINGERPRINT);
+        xml.endTag(null, BUILD_INFO_TAG);
+        xml.endTag(null, DEVICE_INFO_TAG);
+
+        xml.startTag(null, TEST_RESULTS_TAG);
         int count = mAdapter.getCount();
         for (int i = 0; i < count; i++) {
             TestListItem item = mAdapter.getItem(i);
-            if (!item.isTest()) {
-                builder.append(item.title).append('\n');
-            } else {
-                builder.append(item.title)
-                        .append(".....")
-                        .append(getTestResultString(mAdapter.getTestResult(i)))
-                        .append('\n');
-            }
-
-            if (i + 1 < count && !mAdapter.getItem(i + 1).isTest()) {
-                builder.append('\n');
+            if (item.isTest()) {
+                xml.startTag(null, TEST_TAG);
+                xml.attribute(null, "title", item.title);
+                xml.attribute(null, "class-name", item.className);
+                xml.attribute(null, "result", getTestResultString(mAdapter.getTestResult(i)));
+                xml.endTag(null, TEST_TAG);
             }
         }
-        return builder.toString();
+        xml.endTag(null, TEST_RESULTS_TAG);
+
+        xml.endTag(null, TEST_RESULTS_REPORT_TAG);
+        xml.endDocument();
+
+        return outputStream.toString("utf-8");
     }
 
     private String getTestResultString(int testResult) {
-        int resId = 0;
         switch (testResult) {
             case TestResult.TEST_RESULT_PASSED:
-                resId = R.string.pass_result;
-                break;
+                return "pass";
 
             case TestResult.TEST_RESULT_FAILED:
-                resId = R.string.fail_result;
-                break;
+                return "fail";
 
             case TestResult.TEST_RESULT_NOT_EXECUTED:
-                resId = R.string.not_executed_result;
-                break;
+                return "not-executed";
 
             default:
                 throw new IllegalArgumentException("Unknown test result: " + testResult);
         }
-        return mContext.getString(resId);
     }
 }
