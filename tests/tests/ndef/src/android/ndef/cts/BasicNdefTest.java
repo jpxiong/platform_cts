60b284aa63a531ed13dc6d195a1dd8d5d1f9c33f
Ben Dodson <bjdodson@google.com>
2011-07-14 12:04:29 -0700

    test for uri parsing
    Change-Id: Ia70424cec39ceef081f9a819ea6680b6801e3af5

--- tests/tests/ndef/src/android/ndef/cts/BasicNdefTest.java
+++ tests/tests/ndef/src/android/ndef/cts/BasicNdefTest.java
@@ -16,6 +16,7 @@
 
 package android.ndef.cts;
 
+import android.net.Uri;
 import android.nfc.NdefMessage;
 import android.nfc.NdefRecord;
 import android.nfc.FormatException;
@@ -32,6 +33,29 @@ public class BasicNdefTest extends TestCase {
             (byte) 0x6f, (byte) 0x67, (byte) 0x6c, (byte) 0x65, (byte) 0x2e, (byte) 0x63,
             (byte) 0x6f, (byte) 0x6d
     };
+
+    public static final String URI_WWW_STRING = "http://www.nfc.com";
+    public static final byte[] URI_WWW_BYTES = new byte[] {
+            (byte) 0xd1, (byte) 0x01, (byte) 0x08, (byte) 0x55, (byte) 0x01, (byte) 0x6e,
+            (byte) 0x66, (byte) 0x63, (byte) 0x2e, (byte) 0x63, (byte) 0x6f, (byte) 0x6d
+    };
+
+    public static final String URI_TELEPHONE_NUMBER_STRING = "tel:+35891234567";
+    public static final byte[] URI_TELEPHONE_NUMBER_BYTES = new byte[] {
+            (byte) 0xd1, (byte) 0x01, (byte) 0x0d, (byte) 0x55, (byte) 0x05, (byte) 0x2b,
+            (byte) 0x33, (byte) 0x35, (byte) 0x38, (byte) 0x39, (byte) 0x31, (byte) 0x32,
+            (byte) 0x33, (byte) 0x34, (byte) 0x35, (byte) 0x36, (byte) 0x37
+    };
+
+    public void test_uriParser() {
+        NdefRecord wwwRecord = NdefRecord.createUri(Uri.parse(URI_WWW_STRING));
+        System.out.println(new String(wwwRecord.getPayload()));
+        assertByteArrayEquals(URI_WWW_BYTES, wwwRecord.toByteArray());
+
+        NdefRecord telRecord = NdefRecord.createUri(Uri.parse(URI_TELEPHONE_NUMBER_STRING));
+        System.out.println(new String(telRecord.getPayload()));
+        assertByteArrayEquals(URI_TELEPHONE_NUMBER_BYTES, telRecord.toByteArray());
+    }
 
     public void test_parseSmartPoster() throws FormatException {
         NdefMessage msg = new NdefMessage(SMART_POSTER_URL_NO_TEXT);
