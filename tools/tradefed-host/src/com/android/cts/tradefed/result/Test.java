65cd6eff9303eebf0a57c68b42e8a9e44b8771ae
Brett Chabot <brettchabot@android.com>
2011-10-17 18:23:21 -0700

    Add ability to parse test package results from xml.
    Also fix xml generation for stack traces.
    
    Bug 5171576
    
    Change-Id: I3cbcdb69c6887bace2b1bd0610aeba7f1f8b8884

--- tools/tradefed-host/src/com/android/cts/tradefed/result/Test.java
+++ tools/tradefed-host/src/com/android/cts/tradefed/result/Test.java
@@ -20,13 +20,15 @@ import com.android.tradefed.result.TestResult;
 import com.android.tradefed.result.TestResult.TestStatus;
 
 import org.kxml2.io.KXmlSerializer;
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
 
 import java.io.IOException;
 
 /**
  * Data structure that represents a "Test" result XML element.
  */
-class Test {
+class Test extends AbstractXmlPullParser {
 
     static final String TAG = "Test";
     private static final String NAME_ATTR = "name";
@@ -35,6 +37,7 @@ class Test {
     private static final String STARTTIME_ATTR = "starttime";
     private static final String RESULT_ATTR = "result";
     private static final String SCENE_TAG = "FailedScene";
+    private static final String STACK_TAG = "StackTrace";
 
     private String mName;
     private String mResult;
@@ -81,6 +84,26 @@ class Test {
         return mName;
     }
 
+    public String getResult() {
+        return mResult;
+    }
+
+    public String getMessage() {
+        return mMessage;
+    }
+
+    public String getStartTime() {
+        return mStartTime;
+    }
+
+    public String getEndTime() {
+        return mEndTime;
+    }
+
+    public String getStackTrace() {
+        return mStackTrace;
+    }
+
     /**
      * Serialize this object and all its contents to XML.
      *
@@ -98,7 +121,11 @@ class Test {
         if (mMessage != null) {
             serializer.startTag(CtsXmlResultReporter.ns, SCENE_TAG);
             serializer.attribute(CtsXmlResultReporter.ns, MESSAGE_ATTR, mMessage);
-            serializer.text(mStackTrace);
+            if (mStackTrace != null) {
+                serializer.startTag(CtsXmlResultReporter.ns, STACK_TAG);
+                serializer.text(mStackTrace);
+                serializer.endTag(CtsXmlResultReporter.ns, STACK_TAG);
+            }
             serializer.endTag(CtsXmlResultReporter.ns, SCENE_TAG);
         }
         serializer.endTag(CtsXmlResultReporter.ns, TAG);
@@ -145,5 +172,35 @@ class Test {
             return stack.substring(0, firstNewLine);
         }
         return stack;
+    }
+
+    /**
+     * Populates this class with test result data parsed from XML.
+     *
+     * @param parser the {@link XmlPullParser}. Expected to be pointing at start
+     *            of a Test tag
+     */
+    @Override
+    void parse(XmlPullParser parser) throws XmlPullParserException, IOException {
+        if (!parser.getName().equals(TAG)) {
+            throw new XmlPullParserException(String.format(
+                    "invalid XML: Expected %s tag but received %s", TAG, parser.getName()));
+        }
+        setName(getAttribute(parser, NAME_ATTR));
+        mResult = getAttribute(parser, RESULT_ATTR);
+        mStartTime = getAttribute(parser, STARTTIME_ATTR);
+        mEndTime = getAttribute(parser, ENDTIME_ATTR);
+
+        int eventType = parser.next();
+        while (eventType != XmlPullParser.END_DOCUMENT) {
+            if (eventType == XmlPullParser.START_TAG && parser.getName().equals(SCENE_TAG)) {
+                mMessage = getAttribute(parser, MESSAGE_ATTR);
+            } else if (eventType == XmlPullParser.START_TAG && parser.getName().equals(STACK_TAG)) {
+                mStackTrace = parser.nextText();
+            } else if (eventType == XmlPullParser.END_TAG && parser.getName().equals(TAG)) {
+                return;
+            }
+            eventType = parser.next();
+        }
     }
 }
