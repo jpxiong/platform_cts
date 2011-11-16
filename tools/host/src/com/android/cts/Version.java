75eaf7cfa742dc1805110828c9213b36f7deb474
Brian Muramatsu <btmura@google.com>
2011-11-16 11:43:36 -0800

    DO NOT MERGE Update CTS Version to 2.3_r10
    Bug 5616114
    
    Change-Id: I459b8452b47d2ab5efd63c68538a9492db49f985

--- tools/host/src/com/android/cts/Version.java
+++ tools/host/src/com/android/cts/Version.java
@@ -18,7 +18,7 @@ package com.android.cts;
 
 public class Version {
     // The CTS version string
-    private static final String version = "2.3_r9";
+    private static final String version = "2.3_r10";
 
     private Version() {
         // no instances allowed
