88d32a8c7d828d5062be72406eb647e198b78e93
Brian Muramatsu <btmura@google.com>
2011-12-02 10:55:12 -0800

    Support Native Tests in CTS
    Bug 4970300
    
    CTS uses XML files to determine what tests to run. The buildCts.py
    script runs JavaDoc over Java files to create these listings. However,
    this doesn't work for native tests which are executables written in
    C++. This adds a Java tool called cts-native-xml-generator that
    scans the C++ source files to find tests and then generates a
    XML file that buildCts.py can pull in to form the CTS test plans.
    
    The generate XMLs don't use a lot of the existing attributes
    were intended for Java. I have also added a new "testType" attribute,
    because each new test has so far added another boolean like
    "vmHostTest=true" or "isHosttest=true"...
    
    New build rules call cts-native-xml-generator in test_executable.mk.
    Replace calls to include BUILD_EXECUTABLE with BUILD_CTS_EXECUTABLE
    to not only create the executable like usual but to define a rule
    that will call upon cts-native-xml-generator to create the test
    package XML. An additional variable called LOCAL_CTS_TEST_PACKAGE
    is required to give the native tests a parent test package which
    is required by CTS.
    
    CtsNativeTestCase.mk is a Makefile that defines a temporary location
    to store the generated XMLs that are then copied to the final
    location by build/core/cts.mk. This Makefile is included by the main
    CTS Android.mk so that BUILD_CTS_EXECUTABLE can be used throughout
    the project.
    
    CtsTestCaseList also includes CtsNativeTestCase.mk to convert the
    simple test executable names like "CtsNativeTestcases" to its
    full path and the corresponding generated XML's path. These paths
    are then passed to build/core/cts.mk. Both end up in the testcases
    directory. We define two new variables, CTS_TEST_CASES and
    CTS_TEST_XMLS. This is for future planning as discussed later.
    
    Finally, GeeTest and GeeTestResultParser are added to handle
    running the native tests. While TradeFed already has similar
    classes, these two get around the limitations to work on user
    builds and append the package name defined back as
    LOCAL_CTS_TEST_PACKAGE. Integrating these changes back to the
    TradeFed project will need to be done in a separate change.
    
    A lot of this could have been done by adding more to buildCts.py,
    but I didn't want to add to what is a single serial monolithic
    step in the CTS build process. By having a separate rule to
    handle each XML, make can take advantage of the cores on the
    command line rather than sitting there on one core waiting
    for the buildCts.py script to finish.
    
    The CTS_TEST_CASES and CTS_TEST_XMLS are made more general to
    handle copying any file type whether executable or APK to
    the testcases directory. The XMLs could also be for native
    packages or they could be for Java tests too. They are made
    general on purpose so we do not need to modify
    build/core/tasks/cts.mk everytime we add a new test type.
    We just need to update CtsTestCaseList.mk without ever
    having to do a multiproject change. Furthermore, this
    generality allows us to easily convert over the existing
    Java test cases.
    
    Change-Id: I68f784bfedda5182b9795de2e6b2a5b7ed49626e

---
+++ tools/cts-native-xml-generator/src/com/android/cts/nativexml/Generator.java
@@ -0,0 +1,174 @@
+/*
+ * Copyright (C) 2011 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.cts.nativexml;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Generator of TestPackage XML files for native gTests.
+ *
+ * It scours all the C++ source files in a given source directory looking
+ * for test declarations and outputs a XML test listing.
+ */
+class Generator {
+
+    /** Test package name like "android.nativemedia" to group the tests. */
+    private final String mAppPackageName;
+
+    /** Name of the native executable. */
+    private final String mName;
+
+    /** Directory to recursively scan for gTest test declarations. */
+    private final File mSourceDir;
+
+    /** Path to output file or null to just dump to standard out. */
+    private final String mOutputPath;
+
+    Generator(String appPackageName, String name, File sourceDir, String outputPath) {
+        mAppPackageName = appPackageName;
+        mName = name;
+        mSourceDir = sourceDir;
+        mOutputPath = outputPath;
+    }
+
+    public void writePackageXml() throws IOException {
+        OutputStream output = System.out;
+        if (mOutputPath != null) {
+            File outputFile = new File(mOutputPath);
+            File outputDir = outputFile.getParentFile();
+            if (!outputDir.exists() && !outputDir.mkdirs()) {
+                System.err.println("Couldn't make output directory: " + mOutputPath);
+                System.exit(1);
+            }
+            output = new FileOutputStream(outputFile);
+        }
+
+        PrintWriter writer = null;
+        try {
+            writer = new PrintWriter(output);
+            writer.println("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
+            writeTestPackage(writer);
+        } finally {
+            if (writer != null) {
+                writer.close();
+            }
+        }
+    }
+
+    private void writeTestPackage(PrintWriter writer) throws FileNotFoundException {
+        writer.append("<TestPackage appPackageName=\"")
+                .append(mAppPackageName)
+                .append("\" name=\"")
+                .append(mName)
+                .println("\" testType=\"native\" version=\"1.0\">");
+        writeTestSuite(writer);
+        writer.println("</TestPackage>");
+    }
+
+    private void writeTestSuite(PrintWriter writer) throws FileNotFoundException {
+        /*
+         * Given "android.foo.bar.baz"...
+         *
+         * <TestSuite name="android">
+         *   <TestSuite name="foo">
+         *     <TestSuite name="bar">
+         *       <TestSuite name="baz">
+         */
+        Scanner scanner = null;
+        try {
+            scanner = new Scanner(mAppPackageName);
+            scanner.useDelimiter("\\.");
+
+            int numLevels = 0;
+            for (; scanner.hasNext(); numLevels++) {
+                String packagePart = scanner.next();
+                writer.append("<TestSuite name=\"").append(packagePart).println("\">");
+            }
+
+            writeTestCases(writer, mSourceDir);
+
+            for (; numLevels > 0; numLevels--) {
+                writer.println("</TestSuite>");
+            }
+        } finally {
+            if (scanner != null) {
+                scanner.close();
+            }
+        }
+    }
+
+    private void writeTestCases(PrintWriter writer, File dir) throws FileNotFoundException {
+        // Find both C++ files to find tests and directories to look for more tests!
+        File[] files = dir.listFiles(new FilenameFilter() {
+            @Override
+            public boolean accept(File dir, String filename) {
+                return filename.endsWith(".cpp") || new File(dir, filename).isDirectory();
+            }
+        });
+
+        for (int i = 0; i < files.length; i++) {
+            File file = files[i];
+            if (file.isDirectory()) {
+                writeTestCases(writer, file);
+            } else {
+                // Take the test name from the name of the file. It's probably
+                // more accurate to take the name from inside the file...
+                String fileName = file.getName();
+                int extension = fileName.lastIndexOf('.');
+                if (extension != -1) {
+                    fileName = fileName.substring(0, extension);
+                }
+
+                writer.append("<TestCase name=\"").append(fileName).println("\">");
+                writeTests(writer, file);
+                writer.println("</TestCase>");
+            }
+        }
+    }
+
+    // We want to find lines like TEST_F(SLObjectCreationTest, testAudioPlayerFromFdCreation) { ...
+    // and extract the "testAudioPlayerFromFdCreation" as group #1
+    private static final Pattern TEST_REGEX = Pattern.compile("\\s*TEST_F\\(\\w+,\\s*(\\w+)\\).*");
+
+    private void writeTests(PrintWriter writer, File file) throws FileNotFoundException {
+        Scanner scanner = null;
+        try {
+            scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                Matcher matcher = TEST_REGEX.matcher(line);
+                if (matcher.matches()) {
+                    String name = matcher.group(1);
+                    writer.append("<Test name=\"").append(name).println("\" />");
+                }
+            }
+        } finally {
+            if (scanner != null) {
+                scanner.close();
+            }
+        }
+    }
+}
