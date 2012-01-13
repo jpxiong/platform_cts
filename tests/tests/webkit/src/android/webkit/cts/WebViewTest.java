f95948e6c6dfb8e8d47ffdd4af235e8aee41bb31
Victoria Lease <violets@google.com>
2012-01-13 15:56:26 -0800

    Fix CTS WebViewTest#testFindAddress
     Bug: 5866557
    
    The address parser now recognises 929 as an invalid zipcode prefix, and
    rejects addresses from that zipcode. Our test case happened to use that
    zipcode, and was incorrectly expecting an invalid address to be accepted.
    
    I've added a test case to expect failure on this (invalid) zipcode prefix,
    and adjusted the old cases to use a valid zipcode prefix instead.
    
    Change-Id: I3c5a75734c22674e1828dbf33f3a94dda758c238

--- tests/tests/webkit/src/android/webkit/cts/WebViewTest.java
+++ tests/tests/webkit/src/android/webkit/cts/WebViewTest.java
@@ -146,14 +146,16 @@ public class WebViewTest extends ActivityInstrumentationTestCase2<WebViewStubAct
          * http://en.wikipedia.org/wiki/Postal_address#United_States
          * http://www.usps.com/
          */
+        // full address, invalid zip code
+        assertNull(WebView.findAddress("455 LARKSPUR DRIVE CALIFORNIA SPRINGS CALIFORNIA 92926"));
         // full address
-        assertEquals("455 LARKSPUR DRIVE CALIFORNIA SPRINGS CALIFORNIA 92926",
-                WebView.findAddress("455 LARKSPUR DRIVE CALIFORNIA SPRINGS CALIFORNIA 92926"));
+        assertEquals("455 LARKSPUR DRIVE CALIFORNIA SPRINGS CALIFORNIA 92826",
+                WebView.findAddress("455 LARKSPUR DRIVE CALIFORNIA SPRINGS CALIFORNIA 92826"));
         // full address ( with abbreviated street type and state)
-        assertEquals("455 LARKSPUR DR CALIFORNIA SPRINGS CA 92926",
-                WebView.findAddress("455 LARKSPUR DR CALIFORNIA SPRINGS CA 92926"));
+        assertEquals("455 LARKSPUR DR CALIFORNIA SPRINGS CA 92826",
+                WebView.findAddress("455 LARKSPUR DR CALIFORNIA SPRINGS CA 92826"));
         // misspell the state ( CALIFORNIA -> CALIFONIA )
-        assertNull(WebView.findAddress("455 LARKSPUR DRIVE CALIFORNIA SPRINGS CALIFONIA 92926"));
+        assertNull(WebView.findAddress("455 LARKSPUR DRIVE CALIFORNIA SPRINGS CALIFONIA 92826"));
         // without optional zip code
         assertEquals("455 LARKSPUR DR CALIFORNIA SPRINGS CA",
                 WebView.findAddress("455 LARKSPUR DR CALIFORNIA SPRINGS CA"));
