1d89002cb947a741046b3f1e44a9d8890ff75d2c
Brian Muramatsu <btmura@google.com>
2012-06-07 17:30:34 -0700

    Poll for isTouchExplorationEnabled
    This test could possibly fail if isTouchExplorationEnabled is false and
    some setState message has not been processed by
    AccessibilityManagerTest. Maybe it's a thread visibility issue though
    and the test doesn't see the update.
    
    Bug 6537582
    
    Change-Id: I0c691813a0d5e2ad1312bcb04ded2650ae0c0019

--- tests/tests/accessibility/Android.mk
+++ tests/tests/accessibility/Android.mk
@@ -26,6 +26,8 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_PACKAGE_NAME := CtsAccessibilityTestCases
 
+LOCAL_STATIC_JAVA_LIBRARIES := ctsutil
+
 # This test runner sets up/cleans up the device before/after running the tests.
 LOCAL_CTS_TEST_RUNNER := com.android.cts.tradefed.testtype.AccessibilityTestRunner
 
