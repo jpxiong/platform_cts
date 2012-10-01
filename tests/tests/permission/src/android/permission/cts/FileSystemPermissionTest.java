b855e7f0943661358f42274a8103113835a110f8
Nick Kralevich <nnk@google.com>
2012-10-01 14:55:51 -0700

    FileSystemPermissionTest: Add /data/vpnch
    Credit: Jon Sawyer
    
    (cherry picked from commit 08fa9e7cb7c487fdee9fef10eb780a8aa869c3e8)
    
    Change-Id: I5bc9bc8d639603cb37deecbd94e9dc15017d7c6a

--- tests/tests/permission/src/android/permission/cts/FileSystemPermissionTest.java
+++ tests/tests/permission/src/android/permission/cts/FileSystemPermissionTest.java
@@ -402,6 +402,7 @@ public class FileSystemPermissionTest extends AndroidTestCase {
                     "/data/tpapi/etc/tpa",
                     "/data/tpapi/etc/tpa/persistent",
                     "/data/tpapi/user.bin",
+                    "/data/vpnch",
                     "/data/wapi",
                     "/data/wifi",
                     "/data/wimax",
