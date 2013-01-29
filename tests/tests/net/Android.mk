6c01b20bedb2d39bd24df77152b4dd2f3b9df144
Wink Saville <wink@google.com>
2013-01-28 20:00:57 -0800

    Delete frameworks/base/voip use voip-common from frameworks/opt/net/voip
    Change-Id: Ieaba759a0f69b45c4b8839cbed1fe757cdf190c5

--- tests/tests/net/Android.mk
+++ tests/tests/net/Android.mk
@@ -21,7 +21,7 @@ LOCAL_MODULE_TAGS := optional
 # and when built explicitly put it in the data partition
 LOCAL_MODULE_PATH := $(TARGET_OUT_DATA_APPS)
 
-LOCAL_JAVA_LIBRARIES := android.test.runner
+LOCAL_JAVA_LIBRARIES := android.test.runner voip-common
 
 # include CtsTestServer as a temporary hack to free net.cts from cts.stub.
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
