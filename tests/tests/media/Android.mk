9cd57b6389dd8a4342c68951061d1807994c4e57
Jeff Tinker <jtinker@google.com>
2013-09-11 18:51:50 -0700

    Fix MediaDrm cts tests
    Moving the build of the mock MediaDrm plugin back to
    frameworks/av since it can't be installed by cts.
    
    b/10668350
    
    Change-Id: Ic3b3c23de8993577b56ae86a553dcada0b61c606

--- tests/tests/media/Android.mk
+++ tests/tests/media/Android.mk
@@ -28,9 +28,6 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_PACKAGE_NAME := CtsMediaTestCases
 
-LOCAL_JNI_SHARED_LIBRARIES := libmockdrmcryptoplugin
-
-
 # uncomment when dalvik.annotation.Test* are removed or part of SDK
 #LOCAL_SDK_VERSION := current
 
