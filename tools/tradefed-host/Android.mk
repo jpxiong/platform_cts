bbca316bed82b3061e0ccba282cdfb2aa552b664
Stuart Scott <stuartscott@google.com>
2013-11-12 15:05:07 -0800

    Moving include to static.
    bug: 11561456
    
    Change-Id: Id32bf91caa49be3b060a710b5f9fd9fecbba4511

--- tools/tradefed-host/Android.mk
+++ tools/tradefed-host/Android.mk
@@ -22,8 +22,10 @@ LOCAL_JAVA_RESOURCE_DIRS := res
 
 LOCAL_MODULE := cts-tradefed
 LOCAL_MODULE_TAGS := optional
-LOCAL_JAVA_LIBRARIES := ddmlib-prebuilt tradefed-prebuilt hosttestlib ctshostutil ctscommonutilhost
-LOCAL_STATIC_JAVA_LIBRARIES := ctsdeviceinfolib
+LOCAL_JAVA_LIBRARIES := ddmlib-prebuilt tradefed-prebuilt hosttestlib
+LOCAL_STATIC_JAVA_LIBRARIES := ctsdeviceinfolib ctshostutil
+
+LOCAL_ADDITIONAL_DEPENDENCIES := $(LOCAL_PATH)/Android.mk
 
 include $(BUILD_HOST_JAVA_LIBRARY)
 
