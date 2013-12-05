f81b2b965b9f23fd7f2f33b192ed84c396943ecd
Tim Murray <timmurray@google.com>
2013-12-05 15:54:18 -0800

    Add tests for Allocation.
    Change-Id: I818f18fef566bacd4f3e72078a1bac9cbbc6de28

---
+++ tests/tests/rscpp/src/android/cts/rscpp/RSAllocationTest.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2013 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package android.cts.rscpp;
+
+import com.android.cts.stub.R;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.test.AndroidTestCase;
+import android.renderscript.*;
+import android.util.Log;
+
+public class RSAllocationTest extends RSCppTest {
+    static {
+        System.loadLibrary("rscpptest_jni");
+    }
+
+    native boolean typedTest();
+    public void testRSAllocationTypes() {
+        assertTrue(typedTest());
+    }
+
+    native boolean test1DCopy();
+    public void testRSAllocationCopy() {
+        assertTrue(test1DCopy());
+    }
+
+    native boolean testSetElementAt();
+    public void testRSAllocationSetElementAt() {
+        assertTrue(testSetElementAt());
+    }
+
+
+}
