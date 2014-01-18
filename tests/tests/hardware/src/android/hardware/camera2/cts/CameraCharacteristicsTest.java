70fd7dfabb2c5f9eb9dfaacd86f897d438603c14
Igor Murashkin <iam@google.com>
2014-01-17 18:19:06 -0800

    camera2: Add synchronization definitions to enable LIMITED HAL3 devices
    Change-Id: Ic5fda4bdc3dda0d21f4738c81b5742f86e4ef02c

--- tests/tests/hardware/src/android/hardware/camera2/cts/CameraCharacteristicsTest.java
+++ tests/tests/hardware/src/android/hardware/camera2/cts/CameraCharacteristicsTest.java
@@ -1034,5 +1034,28 @@ public class CameraCharacteristicsTest extends AndroidTestCase {
 
         }
     }
+
+    public void testCameraCharacteristicsAndroidSyncMaxLatency() throws Exception {
+        String[] ids = mCameraManager.getCameraIdList();
+        for (int i = 0; i < ids.length; i++) {
+            CameraCharacteristics props = mCameraManager.getCameraCharacteristics(ids[i]);
+            assertNotNull(String.format("Can't get camera characteristics from: ID %s", ids[i]),
+                                        props);
+
+            {
+
+                assertNotNull("Invalid property: android.sync.maxLatency",
+                        props.get(CameraCharacteristics.SYNC_MAX_LATENCY));
+
+                List<Key<?>> allKeys = props.getKeys();
+                assertNotNull(String.format("Can't get camera characteristics keys from: ID %s",
+                        ids[i], props));
+                assertTrue("Key not in keys list: android.sync.maxLatency", allKeys.contains(
+                        CameraCharacteristics.SYNC_MAX_LATENCY));
+
+            }
+
+        }
+    }
 }
 
