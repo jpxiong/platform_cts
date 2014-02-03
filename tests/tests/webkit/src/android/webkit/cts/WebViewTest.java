f9311328eb853b74340af7a3240a04cb986377df
Nicholas Sauer <nicksauer@google.com>
2014-02-03 20:41:55 +0000

    Revert "DO NOT MERGE Use absolute URLs in testRequestFocusNodeHref CTS test."
    This reverts commit 27408515a0388e6aa5df532558e2e84901a422e2.
    
    Change-Id: Idca010fb803da7ed46a823a4bcff01bf56ac296b

--- tests/tests/webkit/src/android/webkit/cts/WebViewTest.java
+++ tests/tests/webkit/src/android/webkit/cts/WebViewTest.java
@@ -1104,10 +1104,9 @@ public class WebViewTest extends ActivityInstrumentationTestCase2<WebViewStubAct
     }
 
     public void testRequestFocusNodeHref() throws Throwable {
-        final String absUrl1 = TestHtmlConstants.HTML_BASE_URL + TestHtmlConstants.HTML_URL1;
-        final String absUrl2 = TestHtmlConstants.HTML_BASE_URL + TestHtmlConstants.HTML_URL2;
-        final String links = "<DL><p><DT><A HREF=\"" + absUrl1 + "\">HTML_URL1</A>"
-                + "<DT><A HREF=\"" + absUrl2 + "\">HTML_URL2</A></DL><p>";
+        final String links = "<DL><p><DT><A HREF=\"" + TestHtmlConstants.HTML_URL1
+                + "\">HTML_URL1</A><DT><A HREF=\"" + TestHtmlConstants.HTML_URL2
+                + "\">HTML_URL2</A></DL><p>";
         mOnUiThread.loadDataAndWaitForCompletion("<html><body>" + links + "</body></html>", "text/html", null);
         getInstrumentation().waitForIdleSync();
 
@@ -1136,7 +1135,7 @@ public class WebViewTest extends ActivityInstrumentationTestCase2<WebViewStubAct
                 return done;
             }
         }.run();
-        assertEquals(absUrl1, handler.getResultUrl());
+        assertEquals(TestHtmlConstants.HTML_URL1, handler.getResultUrl());
 
         // focus on second link
         handler.reset();
@@ -1150,7 +1149,7 @@ public class WebViewTest extends ActivityInstrumentationTestCase2<WebViewStubAct
                 boolean done = false;
                 if (handler.hasCalledHandleMessage()) {
                     if (handler.mResultUrl != null &&
-                            handler.mResultUrl.equals(absUrl2)) {
+                            handler.mResultUrl.equals(TestHtmlConstants.HTML_URL2)) {
                         done = true;
                     } else {
                         handler.reset();
@@ -1162,7 +1161,7 @@ public class WebViewTest extends ActivityInstrumentationTestCase2<WebViewStubAct
                 return done;
             }
         }.run();
-        assertEquals(absUrl2, handler.getResultUrl());
+        assertEquals(TestHtmlConstants.HTML_URL2, handler.getResultUrl());
 
         mOnUiThread.requestFocusNodeHref(null);
     }
