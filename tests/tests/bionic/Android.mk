2830d721b4173ac7a91e4fc8b9cc0cafaaf86460
Christopher Ferris <cferris@google.com>
2014-02-05 13:09:00 -0800

    Native tests use --gtest_list_tests for test list.
    Rewrite the nativescanner to parse the output of --gtest_list_tests.
    
    Modify the rules for the xml test list generation to build the special
    host test list executables and pipe the output to the native scanner.
    
    Modify all of the native tests to create a host list executable.
    
    Remove the known failures list for bionic tests.
    
    Bug: 10914944
    
    Change-Id: Ib4d7da0f260eeef08db6fd083cfeecf92459979b

--- tests/tests/bionic/Android.mk
+++ tests/tests/bionic/Android.mk
@@ -1,26 +1,47 @@
 LOCAL_PATH := $(call my-dir)
 
+test_executable := bionic-unit-tests-cts
+list_executable := $(test_executable)_list
+
 include $(CLEAR_VARS)
 
-cts_src_test_path := bionic/tests
+LOCAL_MODULE := $(test_executable)
 LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE := bionic-unit-tests-cts
+LOCAL_MODULE_PATH := $(TARGET_OUT_DATA)/nativetest
 
 LOCAL_ADDITION_DEPENDENCIES := \
-	$(LOCAL_PATH)/Android.mk \
+    $(LOCAL_PATH)/Android.mk \
 
 LOCAL_SHARED_LIBRARIES += \
-	libstlport \
-	libdl \
+    libstlport \
+    libdl \
 
 LOCAL_WHOLE_STATIC_LIBRARIES += \
-	libBionicTests \
+    libBionicTests \
 
 LOCAL_STATIC_LIBRARIES += \
-	libgtest \
-	libgtest_main \
+    libgtest \
+    libgtest_main \
 
-LOCAL_MODULE_PATH := $(TARGET_OUT_DATA)/nativetest
 LOCAL_CTS_TEST_PACKAGE := android.bionic
-
 include $(BUILD_CTS_EXECUTABLE)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE := $(list_executable)
+
+LOCAL_ADDITION_DEPENDENCIES := \
+    $(LOCAL_PATH)/Android.mk \
+
+# A main without the extra output from the gtest main.
+LOCAL_SRC_FILES := \
+    main.cpp \
+
+LOCAL_LDLIBS += \
+    -lrt \
+
+LOCAL_WHOLE_STATIC_LIBRARIES += \
+    libBionicTests \
+
+include $(BUILD_HOST_NATIVE_TEST)
