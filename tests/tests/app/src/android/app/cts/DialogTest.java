bddfe211e680812147bd22ca68928b40dbf99788
Junjie Hu <junjie.hu@mediatek.com>
2014-03-05 17:40:15 +0800

    Fix CTS android.app package testSetDismissMessage seldom fail issue
    The test case will test dialog's dismiss method by invoking dialogDismiss(d). 
    It start a Handler thread to process the dialog dismiss message. 
    But it did not guarantee the Handler thread has the chance to be executed
     first.Sometimes, the checking callback will occur before the Handler thread's
     execution. It will has timing issue to fail.The fix suggestion is raise the
     priority for thread ht.
    
    Change-Id: Iba87238feb9ba571d40bd0ca3bc8240ec8570f5d
    Signed-off-by: Junjie Hu <junjie.hu@mediatek.com>
    (cherry picked from commit 16c94b8da3c3bd890887931f7ee829c537137ea3)

--- tests/tests/app/src/android/app/cts/DialogTest.java
+++ tests/tests/app/src/android/app/cts/DialogTest.java
@@ -812,10 +812,9 @@ public class DialogTest extends ActivityInstrumentationTestCase2<DialogStubActiv
         assertTrue(d.isShowing());
         assertFalse(mCalledCallback);
         dialogDismiss(d);
+        ht.join(100);
         assertTrue(mCalledCallback);
         assertFalse(d.isShowing());
-
-        ht.join(100);
     }
 
     private void dialogDismiss(final Dialog d) throws Throwable {
