9991aa7ca682fd7fd1a27f034fffc332c5d9f362
Nick Kralevich <nnk@google.com>
2014-07-22 10:36:50 -0700

    Require seccomp if we're running a 3.8 or greater kernel.
    We'll be doing future work on seccomp, and third party applications,
    such as Chrome on Android, need seccomp for sandboxing.
    
    Seccomp can be enabled in the kernel by setting CONFIG_SECCOMP_FILTER=y
    
    For kernels prior to 3.8, we don't require seccomp, as it would
    require backporting work for kernel developers.
    
    Bug: 16409560
    Change-Id: I44b77510a46b2855a34e37b12ad56248d2f89657

--- tests/tests/os/src/android/os/cts/SeccompTest.java
+++ tests/tests/os/src/android/os/cts/SeccompTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2013 The Android Open Source Project
+ * Copyright (C) 2014 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,13 +16,18 @@
 
 package android.os.cts;
 
-public class OSFeatures {
-    static {
-        System.loadLibrary("cts_jni");
-    }
+import junit.framework.TestCase;
 
-    public static native int getNoNewPrivs();
-    public static native int prctlCapBsetRead(int i);
-    public static native boolean hasSeccompSupport();
-    public static native boolean needsSeccompSupport();
+public class SeccompTest extends TestCase {
+
+    public void testSeccomp() {
+        if (CpuFeatures.isArm64Cpu()) {
+            return; // seccomp not supported on arm64
+        }
+        if (OSFeatures.needsSeccompSupport()) {
+            assertTrue("Please enable seccomp support "
+                       + "in your kernel (CONFIG_SECCOMP_FILTER=y)",
+                       OSFeatures.hasSeccompSupport());
+        }
+    }
 }
