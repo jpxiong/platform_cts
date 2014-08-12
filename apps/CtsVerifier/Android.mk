fe5933d26783c757ea50fe5c0e8c6365b9aa2ad7
Harsh Modi <harshmodi@google.com>
2014-08-12 16:52:16 -0700

    Add projection tests to CtsVerifier Bug: 13598201 Change-Id: I8faf7761efe4dfecb164dbf18c01c7cedabe5d11

--- apps/CtsVerifier/Android.mk
+++ apps/CtsVerifier/Android.mk
@@ -21,7 +21,7 @@ LOCAL_MODULE_TAGS := optional
 
 LOCAL_MODULE_PATH := $(TARGET_OUT_DATA_APPS)
 
-LOCAL_SRC_FILES := $(call all-java-files-under, src)
+LOCAL_SRC_FILES := $(call all-java-files-under, src) $(call all-Iaidl-files-under, src)
 
 LOCAL_STATIC_JAVA_LIBRARIES := cts-sensors-tests
 
