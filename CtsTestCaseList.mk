e84939a2d4b8931db7dd197499a07e1146837f0e
dcashman <dcashman@google.com>
2014-08-21 15:33:24 -0700

    Add KeySet key-rotation CTS tests.
    Port the keyset tests found at /platform/frameworks/base/core/tests/coretests/
    to CTS.
    
    Bug: 17391204
    Change-Id: Ib67be62b9cdc973b2401e02d3b43d40b7d178be4

--- CtsTestCaseList.mk
+++ CtsTestCaseList.mk
@@ -29,6 +29,22 @@ cts_security_apps_list := \
     CtsWriteExternalStorageApp \
     CtsMultiUserStorageApp
 
+cts_security_keysets_list := \
+    CtsKeySetTestApp \
+    CtsKeySetPermDefSigningA \
+    CtsKeySetPermDefSigningB\
+    CtsKeySetPermUseSigningA \
+    CtsKeySetPermUseSigningB \
+    CtsKeySetSigningAUpgradeA \
+    CtsKeySetSigningBUpgradeA \
+    CtsKeySetSigningAUpgradeAAndB \
+    CtsKeySetSigningAUpgradeAOrB \
+    CtsKeySetSigningAUpgradeB \
+    CtsKeySetSigningBUpgradeB \
+    CtsKeySetSigningAAndBUpgradeA \
+    CtsKeySetSigningAAndCUpgradeB \
+    CtsKeySetSigningAUpgradeNone
+
 cts_support_packages := \
     CtsAccelerationTestStubs \
     CtsAppTestStubs \
@@ -48,7 +64,8 @@ cts_support_packages := \
     TestDeviceSetup \
     CtsUiAutomatorApp \
     CtsUsbSerialTestApp \
-    $(cts_security_apps_list)
+    $(cts_security_apps_list) \
+    $(cts_security_keysets_list)
 
 cts_external_packages := \
     com.replica.replicaisland \
