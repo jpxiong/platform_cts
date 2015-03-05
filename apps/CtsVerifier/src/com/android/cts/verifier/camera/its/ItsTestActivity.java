ab98adabf15031a9a35dd8c054e120b732ebd80b
Yin-Chia Yeh <yinchiayeh@google.com>
2015-03-05 13:28:53 -0800

    CameraITS: various ITS improvement
    1. run_all_tests.py now don't require a camera id argument.
       The script now iterate through all cameras automatically.
    2. run_all_tests.py will now repeat capturing a image for
       user to visually check if current device placement is good
       for ITS testing. This should help front camera test since
       front camera will be facing test scene.
    3. CtsVerifier will now save ITS test summaries and reported
       errors.
    4. Enable pass button if all cameras on device are legacy level.
    
    Change-Id: Ic8740f65dbaa67b0dc2d2aa8737355c0dffe1b6f

--- apps/CtsVerifier/src/com/android/cts/verifier/camera/its/ItsTestActivity.java
+++ apps/CtsVerifier/src/com/android/cts/verifier/camera/its/ItsTestActivity.java
@@ -27,9 +27,19 @@ import android.hardware.camera2.CameraManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
-import java.util.HashSet;
-import java.util.Arrays;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.HashMap;
+import java.util.Arrays;
+import java.util.List;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import com.android.compatibility.common.util.ResultType;
+import com.android.compatibility.common.util.ResultUnit;
 import com.android.cts.verifier.PassFailButtons;
 import com.android.cts.verifier.R;
 
@@ -41,7 +51,9 @@ import com.android.cts.verifier.R;
  */
 public class ItsTestActivity extends PassFailButtons.Activity {
     private static final String TAG = "ItsTestActivity";
+    private static final String EXTRA_CAMERA_ID = "camera.its.extra.CAMERA_ID";
     private static final String EXTRA_SUCCESS = "camera.its.extra.SUCCESS";
+    private static final String EXTRA_SUMMARY = "camera.its.extra.SUMMARY";
     private static final String ACTION_ITS_RESULT =
             "com.android.cts.verifier.camera.its.ACTION_ITS_RESULT";
 
@@ -50,20 +62,32 @@ public class ItsTestActivity extends PassFailButtons.Activity {
         public void onReceive(Context context, Intent intent) {
             Log.i(TAG, "Received result for Camera ITS tests");
             if (ACTION_ITS_RESULT.equals(intent.getAction())) {
+                String cameraId = intent.getStringExtra(EXTRA_CAMERA_ID);
                 String result = intent.getStringExtra(EXTRA_SUCCESS);
-                String[] parts = result.split("=");
-                if (parts.length != 2) {
-                    Toast.makeText(ItsTestActivity.this,
-                            "Received unknown ITS result string: " + result,
-                            Toast.LENGTH_SHORT).show();
+                String summaryPath = intent.getStringExtra(EXTRA_SUMMARY);
+                if (!mNonLegacyCameraIds.contains(cameraId)) {
+                    Log.e(TAG, "Unknown camera id " + cameraId + " reported to ITS");
+                    return;
                 }
-                String cameraId = parts[0];
-                boolean pass = parts[1].equals("True");
+
+                Log.i(TAG, "ITS summary path is: " + summaryPath);
+                mSummaryMap.put(cameraId, summaryPath);
+                // Create summary report
+                if (mSummaryMap.keySet().containsAll(mNonLegacyCameraIds)) {
+                    StringBuilder summary = new StringBuilder();
+                    for (String id : mNonLegacyCameraIds) {
+                        String path = mSummaryMap.get(id);
+                        appendFileContentToSummary(summary, path);
+                    }
+                    ItsTestActivity.this.getReportLog().setSummary(
+                            summary.toString(), 1.0, ResultType.NEUTRAL, ResultUnit.NONE);
+                }
+                boolean pass = result.equals("True");
                 if(pass) {
                     Log.i(TAG, "Received Camera " + cameraId + " ITS SUCCESS from host.");
                     mITSPassedCameraIds.add(cameraId);
-                    if (mCameraIds != null &&
-                            mITSPassedCameraIds.containsAll(Arrays.asList(mCameraIds))) {
+                    if (mNonLegacyCameraIds != null && mNonLegacyCameraIds.size() != 0 &&
+                            mITSPassedCameraIds.containsAll(mNonLegacyCameraIds)) {
                         ItsTestActivity.this.showToast(R.string.its_test_passed);
                         ItsTestActivity.this.getPassButton().setEnabled(true);
                     }
@@ -73,11 +97,40 @@ public class ItsTestActivity extends PassFailButtons.Activity {
                 }
             }
         }
+
+        private void appendFileContentToSummary(StringBuilder summary, String path) {
+            BufferedReader reader = null;
+            try {
+                reader = new BufferedReader(new FileReader(path));
+                String line = null;
+                do {
+                    line = reader.readLine();
+                    if (line != null) {
+                        summary.append(line);
+                    }
+                } while (line != null);
+            } catch (FileNotFoundException e) {
+                Log.e(TAG, "Cannot find ITS summary file at " + path);
+                summary.append("Cannot find ITS summary file at " + path);
+            } catch (IOException e) {
+                Log.e(TAG, "IO exception when trying to read " + path);
+                summary.append("IO exception when trying to read " + path);
+            } finally {
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (IOException e) {
+                    }
+                }
+            }
+        }
     }
 
     private final SuccessReceiver mSuccessReceiver = new SuccessReceiver();
     private final HashSet<String> mITSPassedCameraIds = new HashSet<>();
-    private String[] mCameraIds = null;
+    // map camera id to ITS summary report path
+    private final HashMap<String, String> mSummaryMap = new HashMap<>();
+    ArrayList<String> mNonLegacyCameraIds = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -96,25 +149,27 @@ public class ItsTestActivity extends PassFailButtons.Activity {
             showToast(R.string.no_camera_manager);
         } else {
             try {
-                mCameraIds = manager.getCameraIdList();
+                String[] cameraIds = manager.getCameraIdList();
+                mNonLegacyCameraIds = new ArrayList<String>();
                 boolean allCamerasAreLegacy = true;
-                for (String id : mCameraIds) {
+                for (String id : cameraIds) {
                     CameraCharacteristics characteristics = manager.getCameraCharacteristics(id);
                     if (characteristics.get(CameraCharacteristics.INFO_SUPPORTED_HARDWARE_LEVEL)
                             != CameraCharacteristics.INFO_SUPPORTED_HARDWARE_LEVEL_LEGACY) {
+                        mNonLegacyCameraIds.add(id);
                         allCamerasAreLegacy = false;
-                        break;
                     }
                 }
                 if (allCamerasAreLegacy) {
                     showToast(R.string.all_legacy_devices);
-                    getPassButton().setEnabled(false);
+                    getPassButton().setEnabled(true);
                 }
             } catch (CameraAccessException e) {
                 Toast.makeText(ItsTestActivity.this,
                         "Received error from camera service while checking device capabilities: "
                                 + e, Toast.LENGTH_SHORT).show();
             }
+            Log.d(TAG, "register ITS result receiver");
             IntentFilter filter = new IntentFilter(ACTION_ITS_RESULT);
             registerReceiver(mSuccessReceiver, filter);
         }
@@ -123,6 +178,7 @@ public class ItsTestActivity extends PassFailButtons.Activity {
     @Override
     protected void onPause() {
         super.onPause();
+        Log.d(TAG, "unregister ITS result receiver");
         unregisterReceiver(mSuccessReceiver);
     }
 
