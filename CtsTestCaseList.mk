6e9ebcb1004243be7d2287a1cf250664ba048eae
Chris Craik <ccraik@google.com>
2015-06-01 17:14:38 -0700

    Add initial atrace host tests
    bug:21559370
    bug:21566233
    
    Change-Id: Ib7501dd1388c49b529911669dee97e52f518ced0

--- CtsTestCaseList.mk
+++ CtsTestCaseList.mk
@@ -183,6 +183,7 @@ CTS_COVERAGE_TEST_CASE_LIST := \
 cts_host_libraries := \
     CtsAdbTests \
     CtsAppSecurityTests \
+    CtsAtraceTestCases \
     CtsDevicePolicyManagerTestCases \
     CtsDumpsysHostTestCases \
     CtsHostJank \
