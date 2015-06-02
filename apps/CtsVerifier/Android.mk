237402032078703a6658c034461cdd33087fa281
leozwang <leozwang@google.com>
2015-06-02 16:23:00 -0700

    Add build number into CTS Verifier.
    Change-Id: I10f98415d89eea50a7b0a652f882200f2eaf062b

--- apps/CtsVerifier/Android.mk
+++ apps/CtsVerifier/Android.mk
@@ -36,6 +36,8 @@ LOCAL_STATIC_JAVA_LIBRARIES := android-ex-camera2 \
 
 LOCAL_PACKAGE_NAME := CtsVerifier
 
+LOCAL_AAPT_FLAGS += --version-name "5.0_r1.91 $(BUILD_NUMBER)"
+
 LOCAL_JNI_SHARED_LIBRARIES := libctsverifier_jni
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
