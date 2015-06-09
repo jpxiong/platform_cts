946ade7c2b6b1cc0b7eb44edfa27d830b8a97d7c
Santos Cordon <santoscordon@google.com>
2015-06-08 19:02:43 -0700

    Add a CTS test to dial addresses ending in "#"
    Also adds a SimpleTelecomTest helper class to simplify writing
    CTS tests.
    
    Bug: 20614281
    Change-Id: I9c643a33c19b0e4cd347ab1d55775524330e00f3

--- tests/tests/telecom/src/android/telecom/cts/MockConnectionService.java
+++ tests/tests/telecom/src/android/telecom/cts/MockConnectionService.java
@@ -19,13 +19,23 @@ package android.telecom.cts;
 import android.telecom.Connection;
 import android.telecom.ConnectionRequest;
 import android.telecom.ConnectionService;
+import android.telecom.PhoneAccount;
 import android.telecom.PhoneAccountHandle;
 import android.telecom.TelecomManager;
 
 import java.util.concurrent.Semaphore;
 
+/**
+ * This is the official ConnectionService for Telecom's CTS App. Since telecom requires that a
+ * CS be registered in the AndroidManifest.xml file, we have to have a single implementation
+ * of a CS and this is it. To test specific CS behavior, tests will implement their own CS and
+ * tell MockConnectionService to forward any method invocations to that test's implementation.
+ * This is set up using {@link #setUp} and should be cleaned up before the end of the test using
+ * {@link #tearDown}.
+ */
 public class MockConnectionService extends ConnectionService {
     private static ConnectionServiceCallbacks sCallbacks;
+    private static ConnectionService sConnectionService;
     private static Object sLock = new Object();
 
     public static abstract class ConnectionServiceCallbacks {
@@ -48,9 +58,32 @@ public class MockConnectionService extends ConnectionService {
         }
     }
 
+    public static PhoneAccount setUp(PhoneAccount phoneAccount, ConnectionService connectionService)
+            throws Exception {
+        synchronized(sLock) {
+            if (sConnectionService != null) {
+                throw new Exception("Mock ConnectionService exists.  Failed to call tearDown().");
+            }
+            sConnectionService = connectionService;
+            return phoneAccount;
+        }
+    }
+
+    public static void tearDown() {
+        synchronized(sLock) {
+            sConnectionService = null;
+        }
+    }
+
     @Override
     public Connection onCreateOutgoingConnection(PhoneAccountHandle connectionManagerPhoneAccount,
             ConnectionRequest request) {
+        synchronized(sLock) {
+            if (sConnectionService != null) {
+                return sConnectionService.onCreateOutgoingConnection(
+                        connectionManagerPhoneAccount, request);
+            }
+        }
         final MockConnection connection = new MockConnection();
         connection.setAddress(request.getAddress(), TelecomManager.PRESENTATION_ALLOWED);
 
@@ -66,6 +99,13 @@ public class MockConnectionService extends ConnectionService {
     @Override
     public Connection onCreateIncomingConnection(PhoneAccountHandle connectionManagerPhoneAccount,
             ConnectionRequest request) {
+        synchronized(sLock) {
+            if (sConnectionService != null) {
+                return sConnectionService.onCreateIncomingConnection(
+                        connectionManagerPhoneAccount, request);
+            }
+        }
+
         final MockConnection connection = new MockConnection();
         connection.setAddress(request.getAddress(), TelecomManager.PRESENTATION_ALLOWED);
 
