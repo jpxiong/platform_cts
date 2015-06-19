0ff952bd359483eb5450fea3724053719d998f4d
Alan Viverette <alanv@google.com>
2015-06-19 12:57:51 -0700

    Miscellaneous CTS tests for new APIs
    - ContextThemeWrapper's new constructor
    - View's scroll indicator APIs
    - ListPopupWindow's window type setter
    - RelativeLayout's layout rule getter
    - CompoundButton's drawable getter
    
    Bug: 21665402
    Bug: 21664698
    Change-Id: I1e919ff78d1dde6b0f3b509b1334b0d28e2209c6

--- tests/tests/widget/src/android/widget/cts/CompoundButtonTest.java
+++ tests/tests/widget/src/android/widget/cts/CompoundButtonTest.java
@@ -188,6 +188,7 @@ public class CompoundButtonTest extends AndroidTestCase {
         // set null drawable
         compoundButton = new MockCompoundButton(mContext);
         compoundButton.setButtonDrawable(null);
+        assertNull(compoundButton.getButtonDrawable());
 
         // set drawable when checkedTextView is GONE
         compoundButton = new MockCompoundButton(mContext);
@@ -197,6 +198,7 @@ public class CompoundButtonTest extends AndroidTestCase {
         assertEquals(StateSet.WILD_CARD, firstDrawable.getState());
 
         compoundButton.setButtonDrawable(firstDrawable);
+        assertSame(firstDrawable, compoundButton.getButtonDrawable());
         assertFalse(firstDrawable.isVisible());
 
         // update drawable when checkedTextView is VISIBLE
@@ -206,6 +208,7 @@ public class CompoundButtonTest extends AndroidTestCase {
         assertEquals(StateSet.WILD_CARD, secondDrawable.getState());
 
         compoundButton.setButtonDrawable(secondDrawable);
+        assertSame(secondDrawable, compoundButton.getButtonDrawable());
         assertTrue(secondDrawable.isVisible());
         // the firstDrawable is not active.
         assertFalse(firstDrawable.isVisible());
