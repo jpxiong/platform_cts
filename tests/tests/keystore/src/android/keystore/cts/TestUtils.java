bd6ffe73777e4aafe20e6e6bced9b68186a99c7c
Alex Klyubin <klyubin@google.com>
2015-07-14 14:47:30 -0700

    CTS tests for entropy consumption by Cipher and Signature.
    This adds CTS tests that assert that the correct amount of entropy
    is consumed by Android Keystore's Cipher encrypt/decrypt operations
    and Signature signing operation.
    
    Unfortunately, due to Bug 22485587 the entropy test for Signature
    primitive is disabled.
    
    Bug: 21936191
    Change-Id: I356abc4f5e0996d7fc555f95d8a0ec02f475eb96

--- tests/tests/keystore/src/android/keystore/cts/TestUtils.java
+++ tests/tests/keystore/src/android/keystore/cts/TestUtils.java
@@ -587,4 +587,38 @@ abstract class TestUtils extends Assert {
         }
         return result;
     }
+
+    static byte[] concat(byte[] arr1, byte[] arr2) {
+        return concat(arr1, 0, (arr1 != null) ? arr1.length : 0,
+                arr2, 0, (arr2 != null) ? arr2.length : 0);
+    }
+
+    static byte[] concat(byte[] arr1, int offset1, int len1,
+            byte[] arr2, int offset2, int len2) {
+        if (len1 == 0) {
+            return subarray(arr2, offset2, len2);
+        } else if (len2 == 0) {
+            return subarray(arr1, offset1, len1);
+        }
+        byte[] result = new byte[len1 + len2];
+        if (len1 > 0) {
+            System.arraycopy(arr1, offset1, result, 0, len1);
+        }
+        if (len2 > 0) {
+            System.arraycopy(arr2, offset2, result, len1, len2);
+        }
+        return result;
+    }
+
+    static byte[] subarray(byte[] arr, int offset, int len) {
+        if (len == 0) {
+            return EmptyArray.BYTE;
+        }
+        if ((offset == 0) && (arr.length == len)) {
+            return arr;
+        }
+        byte[] result = new byte[len];
+        System.arraycopy(arr, offset, result, 0, len);
+        return result;
+    }
 }
