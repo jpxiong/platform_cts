1262cdfc73d91018c2fff97be30c1dbe7f38ed05
Noah Presler <noahp@google.com>
2015-08-13 17:08:19 -0700

    Fixing API level for cts tests with rs cpp api
    b/23161320
    
    Change-Id: Ibc23309abed092e7648168fbf4b80891a44929a6

--- tests/tests/rscpp/librscpptest/Android.mk
+++ tests/tests/rscpp/librscpptest/Android.mk
@@ -50,11 +50,7 @@ LOCAL_CPPFLAGS := -std=c++11
 LOCAL_SHARED_LIBRARIES := libdl liblog
 LOCAL_STATIC_LIBRARIES := libRScpp_static libcutils
 
-ifeq ($(my_32_64_bit_suffix),32)
-    LOCAL_SDK_VERSION := 8
-else
-    LOCAL_SDK_VERSION := 21
-endif
+LOCAL_SDK_VERSION := 21
 
 LOCAL_NDK_STL_VARIANT := stlport_static
 
