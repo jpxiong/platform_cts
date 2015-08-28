c56f73f7969203f591516f6c0f4a6189f188e548
Eric Laurent <elaurent@google.com>
2015-08-28 16:40:30 -0700

    DO NOT MERGE - Add CTS tests for IAudioflinger binder checks.
    Bug: 19573085.
    Bug: 20731946.
    Change-Id: Iedaa5e063052ff6ec428438a1437156ae80dd671

--- tests/tests/security/jni/CtsSecurityJniOnLoad.cpp
+++ tests/tests/security/jni/CtsSecurityJniOnLoad.cpp
@@ -27,6 +27,7 @@ extern int register_android_security_cts_SeccompDeathTestService(JNIEnv*);
 extern int register_android_security_cts_SELinuxTest(JNIEnv*);
 extern int register_android_security_cts_MMapExecutableTest(JNIEnv* env);
 extern int register_android_security_cts_AudioPolicyBinderTest(JNIEnv* env);
+extern int register_android_security_cts_AudioFlingerBinderTest(JNIEnv* env);
 
 jint JNI_OnLoad(JavaVM *vm, void *reserved) {
     JNIEnv *env = NULL;
@@ -72,6 +73,10 @@ jint JNI_OnLoad(JavaVM *vm, void *reserved) {
     }
 
     if (register_android_security_cts_AudioPolicyBinderTest(env)) {
+        return JNI_ERR;
+    }
+
+    if (register_android_security_cts_AudioFlingerBinderTest(env)) {
         return JNI_ERR;
     }
 
