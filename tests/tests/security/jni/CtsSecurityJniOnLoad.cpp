a99ccd9b2069ad63a404c8fe6b613fa37fd908c8
Robert Shih <robertshih@google.com>
2015-09-16 17:12:06 +0000

    DO NOT MERGE ANYWHERE:revert and resubmit to lmp-* branches
    Revert "DO NOT MERGE ANYWHERE: Test whether mediaserver leaks info across invalid binder calls"
    
    This reverts commit ab984239ce7d6cf247930e44b0c7feac08a44c6d.
    
    Bug: 23756261
    Change-Id: Ice15d9d5473d70a65541765612c1a1c9fee77737

--- tests/tests/security/jni/CtsSecurityJniOnLoad.cpp
+++ tests/tests/security/jni/CtsSecurityJniOnLoad.cpp
@@ -28,7 +28,6 @@ extern int register_android_security_cts_SELinuxTest(JNIEnv*);
 extern int register_android_security_cts_MMapExecutableTest(JNIEnv* env);
 extern int register_android_security_cts_AudioPolicyBinderTest(JNIEnv* env);
 extern int register_android_security_cts_AudioFlingerBinderTest(JNIEnv* env);
-extern int register_android_security_cts_MediaPlayerInfoLeakTest(JNIEnv* env);
 extern int register_android_security_cts_AudioEffectBinderTest(JNIEnv* env);
 
 jint JNI_OnLoad(JavaVM *vm, void *reserved) {
@@ -79,10 +78,6 @@ jint JNI_OnLoad(JavaVM *vm, void *reserved) {
     }
 
     if (register_android_security_cts_AudioFlingerBinderTest(env)) {
-        return JNI_ERR;
-    }
-
-    if (register_android_security_cts_MediaPlayerInfoLeakTest(env)) {
         return JNI_ERR;
     }
 
