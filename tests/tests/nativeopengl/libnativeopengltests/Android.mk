c3172b5015a0a2a5ba395864e2367cc7ecec711e
Ying Wang <wangying@google.com>
2015-09-24 10:14:58 -0700

    Don't falsify LOCAL_PATH.
    LOCAL_PATH should be where Android.mk is,
    otherwise the behavior of mmma would be confusing.
    
    Change-Id: If83e865d22e729afe14c7d16d97b439892b75e1b

--- tests/tests/nativeopengl/libnativeopengltests/Android.mk
+++ tests/tests/nativeopengl/libnativeopengltests/Android.mk
@@ -16,26 +16,28 @@
 # This is the shared library included by the JNI test app.
 #
 
-LOCAL_PATH := $(call my-dir)/../standalone/jni/
-REAL_LOCAL_PATH := $(call my-dir)
+LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_ADDITIONAL_DEPENDENCIES := $(REAL_LOCAL_PATH)/Android.mk
-
 LOCAL_MODULE := libnativeopengltests
 
 # Don't include this package in any configuration by default.
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_C_INCLUDES := $(JNI_H_INCLUDE) \
-    external/gtest/include \
+my_src_dir := ../standalone/jni
 
-LOCAL_SRC_FILES := \
+LOCAL_C_INCLUDES := \
+    $(LOCAL_PATH)/$(my_src_dir) \
+    external/gtest/include
+
+# The sources are in ../standalone/jni/
+LOCAL_SRC_FILES := $(addprefix $(my_src_dir)/, \
         register.cpp \
         GLTestHelper.cpp \
         tests/GLTest_test.cpp \
         tests/EGLCleanup_test.cpp \
-        tests/EGLCreateContext_test.cpp
+        tests/EGLCreateContext_test.cpp \
+        )
 
 LOCAL_CXX_STL := libc++
 LOCAL_SHARED_LIBRARIES := libEGL \
@@ -44,5 +46,7 @@ LOCAL_SHARED_LIBRARIES := libEGL \
                           liblog \
 
 LOCAL_STATIC_LIBRARIES := libgtest
+
+my_src_dir :=
 
 include $(BUILD_SHARED_LIBRARY)
